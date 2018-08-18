07cbcb0a44ba3ee0badffe9f07f13c074175227d
Bruno Ukita <brunoukita@gmail.com>
2018-08-18 15:53:21 -0300

    add users list component

--- frontend/src/components/user/index.js
+++ frontend/src/components/user/index.js
@@ -1,9 +1,11 @@
 import userComponent from "./user.component";
+import userListComponent from "./user-list/user-list.component";
 
 const userModule = angular.module("app.user", []);
 
 // loading components, services, directives, specific to this module.
 userModule.component("appUser", userComponent);
+userModule.component("appUserList", userListComponent);
 
 // export this module
 export default userModule;
